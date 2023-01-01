ba908aad8f022a74fb8956e7826639dcbc75b562
ALI KOMURCU <alikomurcu32430@gmail.com>
2023-01-01 16:46:52 +0300

    Isomoprh logic will be changed

--- 205-isomorphic_strings/205-isomorphic_strings.cpp
+++ 205-isomorphic_strings/205-isomorphic_strings.cpp
@@ -9,11 +9,29 @@ using namespace std;
 class Solution {
 public:
     bool isIsomorphic(string s, string t) {
+        // create a map from s to t
         unordered_map<char, char> map;
 
-        for (int i = 0; i < s.size(); i++) {
-
+        int szS = s.size();
+        int szT = t.size();
+        string t2 = s;
+        if (szS != szT) return false;
+        for(int i = 0; i < szS; i++)
+        {
+            const char c = s[i];
+            const char d = t[i];
+            if (map.find(d) != map.end())
+            {
+                if (map[d] != t2[i]) return false;
+            }
+            else
+            {
+                map[d] = c;
+            }
+            std::replace(s.begin(), s.end(), c, d);
         }
+        if (s.compare(t) == 0) return true;
+        else return false;
     }
 };
 
